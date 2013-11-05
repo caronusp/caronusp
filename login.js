8dfb9b4514be67fe96bbac7398f95763ecd6d81f
lucasmpinheiro <lucasmpinheiro@gmail.com>
2013-11-05 10:18:27 -0200

    Update login.js

--- login.js
+++ login.js
@@ -2,14 +2,14 @@ function validarLogin() {
     var email=document.forms["Login"]["email"].value;
     var senha=document.forms["Login"]["senha"].value;
     if (email==null || email=="" || senha==null || senha=="") {
-        document.getElementById("alerta").innerHTML="Campo obrigatÃ³rio nÃ£o preenchido.";
+        document.getElementById("alerta").innerHTML="Campo obrigatório não preenchido.";
         //alert("Campo obrigatÃ³rio nÃ£o preenchido.");
         return false;
     }
     var atpos=email.indexOf("@");
     var dotpos=email.lastIndexOf(".");
     if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length) {
-        document.getElementById("alerta").innerHTML="EndereÃ§o de e-mail nÃ£o Ã© vÃ¡lido.";
+        document.getElementById("alerta").innerHTML="Endereço de e-mail não é válido.";
         //alert("EndereÃ§o de e-mail nÃ£o Ã© vÃ¡lido.");
         return false;
     }
@@ -19,7 +19,7 @@ function validarLogin() {
         session.setAttribute("user_name", email);
         return true;
     } else {
-        document.getElementById("alerta").innerHTML="UsuÃ¡rio e/ou senha invÃ¡lidos!";
+        document.getElementById("alerta").innerHTML="Usuário e/ou senha inválidos!";
         return false;
     } 
 }
