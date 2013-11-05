1b37763db95af89fd25821b88bb96f2de813a05c
Vitor Trivilin <vitorlando@gmail.com>
2013-11-05 09:29:56 -0200

    Update UsuarioData.java

--- UsuarioData.java
+++ UsuarioData.java
@@ -1,74 +1,145 @@
 package classes.data;
 
-import java.sql.*;
-import java.util.*;
-import utils.Transacao;
+import java.sql.Date;
 
-public class ContatoData {
+/**
+ *
+ * @author Vitor Henrique
+ */
+public class UsuarioDO {
+  private int _idUsuario;
+  private String _strNome; 
+  private int _numCPF;
+  private String _strEmail;// VARCHAR(45) NULL,
+  private String _strSenha;// VARCHAR(20) NULL,
+  private String _strCEP;// VARCHAR(20) NULL,
+  private String _strRua;// VARCHAR(45) NULL,
+  private int _numNumeroCasa;// INTEGER UNSIGNED NULL,
+  private String _strComplemento;// VARCHAR(20) NULL,
+  private int _numTelefone;// INTEGER UNSIGNED NULL,
+  private String _strTipo;// CHAR NULL,
+  private String _strSexo;// CHAR NULL,
+  private Date _dateNascimento;// DATE NULL,
+  private String _strProfissao;// VARCHAR(45) NULL,
+  private String _strEntidade;// VARCHAR(45) NULL,
+  
+   public String getNome() {
+     return _strNome;
+  } // getEmail
 
-  public void incluir(ContatoDO contato, Transacao tr) throws Exception {
-     Connection con = tr.obterConexao();
-     String sql = "insert into Agenda (nome, telefone) values (?, ?)";
-     PreparedStatement ps = con.prepareStatement(sql);
-     ps.setString(1, contato.getNome());
-     ps.setString(2, contato.getTelefone());
-     int result = ps.executeUpdate();
-  }
+  public void setNome(String Nome) {
+    _strNome = Nome;
+  } // setEmail
+  
+    public int getId() {
+     return _idUsuario;
+  } // getId
 
-  public void excluir(ContatoDO contato, Transacao tr) throws Exception {
-     excluir(contato.getId(), tr);
-  } // excluir
+  public void setId(int id) {
+    _idUsuario = id;
+  } // setId
+  
+      public int getCPF() {
+     return _numCPF;
+  } // getCPF
 
-  public void excluir (int idobj, Transacao tr) throws Exception {
-     Connection con = tr.obterConexao();
-     String sql = "delete from Agenda where id=?";
-     PreparedStatement ps = con.prepareStatement(sql);
-     String id = ""+idobj;
-     ps.setString(1, id);
-     int result = ps.executeUpdate();
-  } // excluir 
+  public void setCPF(int CPF) {
+    _numCPF = CPF;
+  } // setCPF
+  
+        public String getEmail() {
+     return _strEmail;
+  } // getEmail
 
-  public void atualizar(ContatoDO contato, Transacao tr) throws Exception {
-     Connection con = tr.obterConexao();
-     String sql = "update Agenda set nome=?, telefone=? where id=?";
-     PreparedStatement ps = con.prepareStatement(sql);
-     ps.setString(1, contato.getNome());
-     ps.setString(2, contato.getTelefone());
-	 ps.setInt(3, contato.getId());
-     int result = ps.executeUpdate();
-  } // atualizar
+  public void setEmail(String Email) {
+    _strEmail = Email;
+  } // setEmail
+  
+        public String getSenha() {
+     return _strSenha;
+  } // getSenha
 
-  public ContatoDO buscar(int idobj, Transacao tr) throws Exception {
-     Connection con = tr.obterConexao();
-     String sql = "select * from Agenda where  id=?";
-     PreparedStatement ps = con.prepareStatement(sql);
-     ps.setInt(1, idobj);
-     ResultSet rs = ps.executeQuery();
-     rs.next();
-     ContatoDO contato = new ContatoDO();
-     contato.setId (rs.getInt("id"));
-     contato.setNome (rs.getString("nome"));
-     contato.setTelefone(rs.getString("telefone"));
-     return contato;
-  } // buscar
+  public void setSenha(String Email) {
+    _strSenha = Email;
+  } // setSenhaCPF
+  
+        public String getCEP() {
+     return _strCEP;
+  } // getCEP
 
-  public Vector pesquisarPorNome(String nome, Transacao tr) throws Exception {
-     Connection con = tr.obterConexao();
-     String sql = "select * from Agenda where nome like ?";
-     PreparedStatement ps = con.prepareStatement(sql);
-     ps.setString(1, nome);
-     ResultSet rs = ps.executeQuery();
-     System.out.println("query executada");
-     Vector contatos = new Vector();
-     while (rs.next()) {
-        ContatoDO c = new ContatoDO();
-        c.setId (rs.getInt("id"));
-        c.setNome (rs.getString("nome"));
-        System.out.println(" got " + c.getNome());
-        c.setTelefone(rs.getString("telefone"));
-        contatos.add(c);
-     }
-     return contatos;
-  } // pesquisarPorNome
+  public void setCEP(String CEP) {
+    _strCEP = CEP;
+  } // setCEP
+  
+        public String getRua() {
+     return _strRua;
+  } // getRua
 
-} // ContatoData
+  public void setRua(String Rua) {
+    _strRua = Rua;
+  } // setRua
+  
+        public int getNcasa() {
+     return _numNumeroCasa;
+  } // getNcasa
+
+  public void setNcasa(int Ncasa) {
+    _numNumeroCasa = Ncasa;
+  } // setNcasa
+  
+        public String getComplemento() {
+     return _strComplemento;
+  } // getComplemento
+
+  public void setComplemento(String Complemento) {
+    _strComplemento = Complemento;
+  } // setComplemento
+  
+        public int getTelefone() {
+     return _numTelefone;
+  } // getTelefone
+
+  public void setTelefone(int Telefone) {
+    _numTelefone = Telefone;
+  } // setTelefone
+  
+        public String getTipo() {
+     return _strTipo;
+  } // getTipo
+
+  public void setTipo(String Tipo) {
+    _strTipo = Tipo;
+  } // setId
+  
+         public String getSexo() {
+     return _strSexo;
+  } // getSexo
+
+  public void setSexo(String Sexo) {
+    _strSexo = Sexo;
+  } // setSexo
+  
+         public Date getDatanascimento() {
+     return _dateNascimento;
+  } // getDatanascimento
+
+  public void setDatanascimento(Date Datanascimento) {
+    _dateNascimento = Datanascimento;
+  } // setDatanascimento
+  
+         public String getProfissao() {
+     return _strProfissao;
+  } // getProfissao
+
+  public void setProfissao(String Profissao) {
+    _strProfissao = Profissao;
+  } // setProfissao
+  
+         public String getEntidade() {
+     return _strEntidade;
+  } // getEntidade
+
+  public void setEntidade(String Entidade) {
+    _strEntidade = Entidade;
+  } // setEntidade
+}
