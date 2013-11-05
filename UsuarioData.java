f084952317218ed18f43d3bdcd326afda90c43d2
Vitor Trivilin <vitorlando@gmail.com>
2013-11-05 09:39:01 -0200

    Update UsuarioData.java

--- UsuarioData.java
+++ UsuarioData.java
@@ -1,145 +1,123 @@
 package classes.data;
 
-import java.sql.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Vector;
+import utils.Transacao;
 
 /**
  *
  * @author Vitor Henrique
  */
-public class UsuarioDO {
-  private int _idUsuario;
-  private String _strNome; 
-  private int _numCPF;
-  private String _strEmail;// VARCHAR(45) NULL,
-  private String _strSenha;// VARCHAR(20) NULL,
-  private String _strCEP;// VARCHAR(20) NULL,
-  private String _strRua;// VARCHAR(45) NULL,
-  private int _numNumeroCasa;// INTEGER UNSIGNED NULL,
-  private String _strComplemento;// VARCHAR(20) NULL,
-  private int _numTelefone;// INTEGER UNSIGNED NULL,
-  private String _strTipo;// CHAR NULL,
-  private String _strSexo;// CHAR NULL,
-  private Date _dateNascimento;// DATE NULL,
-  private String _strProfissao;// VARCHAR(45) NULL,
-  private String _strEntidade;// VARCHAR(45) NULL,
-  
-   public String getNome() {
-     return _strNome;
-  } // getEmail
+public class UsuarioData {
+    
+  public void incluir(UsuarioDO usuario, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "insert into Usuario (strnome, numCPF ,strEmail, strSenha ,strCEP, strRua, numNumeroCasa, strComplemento, numTelefone, charTipo, charSexo, dateNascimento, strProfissao, strEntidade) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+     PreparedStatement ps = con.prepareStatement(sql);
+     ps.setString(1, usuario.getNome());
+     ps.setInt(2, usuario.getCPF());
+     ps.setString(3, usuario.getEmail());
+     ps.setString(4, usuario.getSenha());
+     ps.setString(5, usuario.getCEP());
+     ps.setString(6, usuario.getRua());
+     ps.setInt(7, usuario.getNcasa());
+     ps.setString(8, usuario.getComplemento());
+     ps.setInt(9, usuario.getTelefone());
+     ps.setString(10, usuario.getTipo()); //tipo é um char...
+     ps.setString(11, usuario.getSexo());//sexo é um char...
+     ps.setDate(12, usuario.getDatanascimento());
+     ps.setString(13, usuario.getProfissao());
+     ps.setString(14, usuario.getEntidade());
+     /*     
+     
+ */
+     
+     int result = ps.executeUpdate();
+  }
 
-  public void setNome(String Nome) {
-    _strNome = Nome;
-  } // setEmail
-  
-    public int getId() {
-     return _idUsuario;
-  } // getId
+  public void excluir(UsuarioDO usuario, Transacao tr) throws Exception {
+     excluir(usuario.getId(), tr);
+  } // excluir
 
-  public void setId(int id) {
-    _idUsuario = id;
-  } // setId
-  
-      public int getCPF() {
-     return _numCPF;
-  } // getCPF
+  public void excluir (int idobj, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "delete from Usuario where idUsuario=?";
+     PreparedStatement ps = con.prepareStatement(sql);
+     String id = ""+idobj;
+     ps.setString(1, id);
+     int result = ps.executeUpdate();
+  } // excluir 
 
-  public void setCPF(int CPF) {
-    _numCPF = CPF;
-  } // setCPF
-  
-        public String getEmail() {
-     return _strEmail;
-  } // getEmail
+  public void Alterar(UsuarioDO usuario, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "update Usuario set numCPF=? strEmail=? strSenha=? strCEP=? strRua=? numNumeroCasa=? strComplemento=? numTelefone=? charTipo=? charSexo=? dateNascimento=? strProfissao=? strEntidade=? strNome= ? where idUsuario=?";
+     PreparedStatement ps = con.prepareStatement(sql);
+     ps.setInt(1, usuario.getCPF());
+     ps.setString(2, usuario.getEmail());
+     ps.setString(3, usuario.getSenha());
+     ps.setString(4, usuario.getCEP());
+     ps.setString(5, usuario.getRua());
+     ps.setInt(6, usuario.getNcasa());
+     ps.setString(7, usuario.getComplemento());
+     ps.setInt(8, usuario.getTelefone());
+     ps.setString(9, usuario.getTipo()); //tipo é um char...
+     ps.setString(10, usuario.getSexo());//sexo é um char...
+     ps.setDate(11, usuario.getDatanascimento());
+     ps.setString(12, usuario.getProfissao());
+     ps.setString(13, usuario.getEntidade());
+     ps.setString(14, usuario.getNome());
+     ps.setInt(15, usuario.getId());
+     int result = ps.executeUpdate();
+  } // alterar
 
-  public void setEmail(String Email) {
-    _strEmail = Email;
-  } // setEmail
-  
-        public String getSenha() {
-     return _strSenha;
-  } // getSenha
+  public UsuarioDO buscar(int idobj, Transacao tr) throws Exception {
+     Connection con = tr.obterConexao();
+     String sql = "select * from Usuario where  idUsuario=?";
+     PreparedStatement ps = con.prepareStatement(sql);
+     ps.setInt(1, idobj);
+     ResultSet rs = ps.executeQuery();
+     rs.next();
+     UsuarioDO usuario = new UsuarioDO();
+     usuario.setId (rs.getInt("idUsuario"));
+     usuario.setCPF (rs.getInt("intCPF"));
+     usuario.setEmail (rs.getString("strEmail"));
+     usuario.setSenha (rs.getString("strSenha"));
+     usuario.setRua (rs.getString("strRua"));
+     usuario.setNcasa (rs.getInt("numNumeroCasa"));
+     usuario.setComplemento (rs.getString("strComplemento"));
+     usuario.setTelefone (rs.getInt("intTelefone"));
+     usuario.setTipo (rs.getString("charTipo"));
+     usuario.setSexo (rs.getString("charSexo"));
+     usuario.setDatanascimento (rs.getDate("dateNascimento"));
+     usuario.setProfissao (rs.getString("strProfissão"));
+     usuario.setEntidade (rs.getString("strEntidade"));
+     
+     return usuario;
+  } // buscar
 
-  public void setSenha(String Email) {
-    _strSenha = Email;
-  } // setSenhaCPF
-  
-        public String getCEP() {
-     return _strCEP;
-  } // getCEP
 
-  public void setCEP(String CEP) {
-    _strCEP = CEP;
-  } // setCEP
+  //trabalhar nesse daqui:
   
-        public String getRua() {
-     return _strRua;
-  } // getRua
-
-  public void setRua(String Rua) {
-    _strRua = Rua;
-  } // setRua
   
-        public int getNcasa() {
-     return _numNumeroCasa;
-  } // getNcasa
-
-  public void setNcasa(int Ncasa) {
-    _numNumeroCasa = Ncasa;
-  } // setNcasa
-  
-        public String getComplemento() {
-     return _strComplemento;
-  } // getComplemento
-
-  public void setComplemento(String Complemento) {
-    _strComplemento = Complemento;
-  } // setComplemento
-  
-        public int getTelefone() {
-     return _numTelefone;
-  } // getTelefone
-
-  public void setTelefone(int Telefone) {
-    _numTelefone = Telefone;
-  } // setTelefone
-  
-        public String getTipo() {
-     return _strTipo;
-  } // getTipo
-
-  public void setTipo(String Tipo) {
-    _strTipo = Tipo;
-  } // setId
-  
-         public String getSexo() {
-     return _strSexo;
-  } // getSexo
-
-  public void setSexo(String Sexo) {
-    _strSexo = Sexo;
-  } // setSexo
-  
-         public Date getDatanascimento() {
-     return _dateNascimento;
-  } // getDatanascimento
-
-  public void setDatanascimento(Date Datanascimento) {
-    _dateNascimento = Datanascimento;
-  } // setDatanascimento
-  
-         public String getProfissao() {
-     return _strProfissao;
-  } // getProfissao
-
-  public void setProfissao(String Profissao) {
-    _strProfissao = Profissao;
-  } // setProfissao
-  
-         public String getEntidade() {
-     return _strEntidade;
-  } // getEntidade
-
-  public void setEntidade(String Entidade) {
-    _strEntidade = Entidade;
-  } // setEntidade
+//  public Vector pesquisar
+//        (String nome, Transacao tr) throws Exception {
+//     Connection con = tr.obterConexao();
+//     String sql = "select * from Agenda where nome like ?";
+//     PreparedStatement ps = con.prepareStatement(sql);
+//     ps.setString(1, nome);
+//     ResultSet rs = ps.executeQuery();
+//     System.out.println("query executada");
+//     Vector contatos = new Vector();
+//     while (rs.next()) {
+//        ContatoDO c = new ContatoDO();
+//        c.setId (rs.getInt("id"));
+//        c.setNome (rs.getString("nome"));
+//        System.out.println(" got " + c.getNome());
+//        c.setTelefone(rs.getString("telefone"));
+//        contatos.add(c);
+//     }
+//     return contatos;
+//  } // pesquisarPorNome
 }
