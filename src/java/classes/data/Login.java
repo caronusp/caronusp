037baea74c164e2563f98ca83b2bc98354de9f02
caronusp <contato.caronusp@gmail.com>
2013-11-12 15:11:58 -0200

    Update Login.java

--- src/java/classes/data/Login.java
+++ src/java/classes/data/Login.java
@@ -18,7 +18,7 @@ public class Login {
     static final String AB = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     static Random rnd = new Random();
 
-    String randomString( int len ) {
+    private String randomString( int len ) {
         StringBuilder sb = new StringBuilder( len );
         for( int i = 0; i < len; i++ ) 
            sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
